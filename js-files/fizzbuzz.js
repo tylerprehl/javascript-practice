71b0db2dfe14c010c0eaad76bffc43c7dc01c144
Tyler Prehl <tyler@prehl.us>
2023-07-17 16:15:32 -0400

    Complete Problem Solving activity - FizzBuzz

---
+++ js-files/fizzbuzz.js
@@ -0,0 +1,12 @@
+/* FizzBuzz */
+let fizzbuzz = (n) => {
+    for (let i=1; i<=n; i++) {
+        if (i%3 === 0 && i%5 === 0) { console.log("FizzBuzz"); }
+        else if (i%3 === 0) { console.log("Fizz"); }
+        else if (i%5 === 0) { console.log("Buzz"); }
+        else { console.log(i); }
+    }
+}
+
+let number = Number(prompt("Please enter the number you would like FizzBuzz up to: "));
+fizzbuzz(number);
