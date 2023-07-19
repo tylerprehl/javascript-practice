6933b023b8044e9e27dd43fa2701aab932ce2034
Tyler Prehl <tyler@prehl.us>
2023-07-18 20:56:04 -0400

    Add named function and function (e) info to events.js

--- js-files/events.js
+++ js-files/events.js
@@ -12,3 +12,25 @@ const btn3 = document.querySelector('#btn3-eventListener');
 btn3.addEventListener('click', () => {
   alert("Clicked button 3");
 });
+
+
+/* USE NAMED FUNCTION */
+const btn4 = document.querySelector('#btn4-random');
+
+function alertFunction(e) {
+    alert("YAY! YOU DID IT!");
+}
+
+// Method 2
+// btn.onclick = alertFunction;
+
+// Method 3
+// btn.addEventListener('click', alertFunction);
+
+
+/* ACCESS INFO ABOUT THE EVENT */
+btn4.addEventListener('click', function (e) {
+    // console.log(e); // logs ALL e properties
+    // console.log(e.target); // logs just the DOM node clicked
+    e.target.style.background = 'blue'; // turns background of button blue
+});
