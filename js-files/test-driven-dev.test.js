b76b92a2e8bbbe967b2201b03181bd1a67ba8abe
Tyler Prehl <tyler@prehl.us>
2023-11-28 22:03:50 -0500

    Fill out Jest test file to test all functions in teest-driven-development-functions.js

--- js-files/test-driven-dev.test.js
+++ js-files/test-driven-dev.test.js
@@ -2,6 +2,14 @@
 
 import * as TestFunctions from './test-driven-development-functions';
 
+it('Test Capitalize', () => {
+  expect(TestFunctions.capitalize('something cool')).toBe('Something cool');
+});
+
+it('Test Reverse String', () => {
+  expect(TestFunctions.reverseString('something cool')).toBe('looc gnihtemos');
+});
+
 it('Test Calculator Add', () => {
   expect(TestFunctions.calculator(1, '+', 1)).toBe(2);
 });
